77a08f9a5c7bd01662013d080574051dae04f863
bea93 <bmerinomacia@gmail.com>
2021-04-21 21:26:09 +0200

    21/04/2021 Función inicio de sesión

--- index.php
+++ index.php
@@ -1,66 +1,12 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Ejercicio 1</title>
-    </head>
-    <body>
-        <h1>Aplicación Login Logout POO Multicapa</h1>
-        <?php
-        /**
-          @author: Bea Merino Macía
-          @since: 07/04/2021
-          @description: Conexión a la base de datos con la cuenta usuario y tratamiento de errores.
-         */
-        
-        //Fichero de configuración de la BBDD
-        require_once 'config/configDB.php';
-        
-        try {
-            //Objeto PDO con los datos de conexión
-            $miBD = new PDO(DNS,USER,PASSWORD);
+<?php 
+require_once 'config/config.php';
 
-            //Habilito las excepciones
-            $miBD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
-            //Mensaje que se mostrará si la conexión se realiza correctamente
-            echo "<h2 style='color: green'>Conexión realizada correctamente</h2>" . "<br>";
-            
-            //Array que contiene los atributos del PDO::ATTR 
-            $atributos = [ 
-                "AUTOCOMMIT", 
-                "ERRMODE", 
-                "CASE", 
-                "CLIENT_VERSION", 
-                "CONNECTION_STATUS",
-                "ORACLE_NULLS", 
-                "PERSISTENT", 
-                "SERVER_INFO", 
-                "SERVER_VERSION"
-            ];
-            
-            echo "<h2>Atributos de la conexión</h2>";
-            //Foreach para recorrer el array de atributos
-            foreach ($atributos as $val) { 
-                echo "PDO::ATTR_$val: ";
-                //Mensaje de salida
-                echo $miBD->getAttribute(constant("PDO::ATTR_$val")) . "<br>"; 
-            }
-            //Captura de la excepción
-        } catch (PDOException $mensajeError) { 
-            //Mensaje de salida
-            echo "<p style='color: red'>Error </p>" . $mensajeError->getMessage() . "<br>"; 
-            //Código del error
-            echo "<p style='color: red'>Codigo del error </p>" . $mensajeError->getCode() . "<br>"; 
-        } finally {
-            //Cerramos la conexion
-            unset($miBD); 
-        }
-        ?>
-    </body>
-</html>
+session_start();
+
+require_once 'config/configDB.php';
+
+if(isset($_SESSION['paginaEnCurso']) && $_SESSION['paginaEnCurso']!==$controladores["login"]){ // Si el usuario ha solicitado otra pagina distinta del login
+    require_once $_SESSION['paginaEnCurso']; // Incluimos el controlador de la pagina solicitada almacenado en la sesion
+} else { 
+    require_once $controladores["login"]; // Por defecto, la primera vez que el usuario entra en la pagina cargamos el controlador del login
+}
---
+++ view/vInicio.php
@@ -0,0 +1,22 @@
+<h2 class="fh5co-heading animate-box" data-animate-effect="fadeInLeft">Inicio</h2>
+
+<article>
+    <!--Mensaje de bienvenida cuando el usuario se loguea correctamente. Muestra la descripción del usuario -->
+    <h3>¡Bienvenid@ <?php echo $descUsuario; ?>!</h3>
+    <?php
+    //Si la última conexión del usuario es null se muestra un mensaje de conexión por primera vez
+    if ($numConexiones === 0) {
+        echo "<h3>Esta es la primera vez que te conectas.</h3>";
+    } else {
+        ?>
+        <!-- Si la última conexión no es null, se muestra la fecha y el número de conexiones-->
+        <h3>Usted se ha conectado <?php echo $numConexiones . " veces"; ?></h3>
+        <h3>Su última conexión fue el día <?php echo date('d/m/Y', $ultimaConexion); ?> 
+            a las <?php echo date('H:i:s', $ultimaConexion); ?></h3>
+    <?php } ?>
+</article>
+<div>
+    <form name="logout" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <button class="logout" type="submit" name='cerrarSesion'>Cerrar Sesión</button>
+    </form>
+</div>

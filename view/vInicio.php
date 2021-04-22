c04a3a878309cbaece5dcd8b7689d8a3c27e8bb6
bea93 <bmerinomacia@gmail.com>
2021-04-22 13:53:50 +0200

    22/04/2021 Mejora del mensaje de inicio de sesión

--- view/vInicio.php
+++ view/vInicio.php
@@ -2,9 +2,8 @@
 <article>
     <!--Mensaje de bienvenida cuando el usuario se loguea correctamente. Muestra la descripción del usuario -->
     <h3>¡Bienvenid@ <?php echo $descUsuario; ?>!</h3>
-    <h2 class="bienvenida"><?php echo $aLang[$_COOKIE['idioma']]['welcome'] ?> </h2>
-        <h4><?php echo ($numConexiones > 1) ? "Te has conectado " . $numConexiones . " veces."  : "Esta es la primera vez que te conectas." ?></h4>
-        <?php echo ($ultimaConexion != null) ? "<h4>La última conexión fue el " . date('d/m/Y', $ultimaConexion) . " a las " . date('H:i:s', $ultimaConexion) . "</h4>" : null; ?>
+    <h4><?php echo ($numConexiones > 1) ? "Te has conectado " . $numConexiones . " veces."  : "Esta es la primera vez que te conectas." ?></h4>
+    <?php echo ($ultimaConexion != null) ? "<h4>La última conexión fue el " . date('d/m/Y', $ultimaConexion) . " a las " . date('H:i:s', $ultimaConexion) . "</h4>" : null; ?>
 </article>
 <div>
     <form name="logout" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">

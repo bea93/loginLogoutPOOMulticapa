1f7487d61d9a41f7a1cfdcda78f3cc62c7c44986
bea93 <bmerinomacia@gmail.com>
2021-05-03 13:49:42 +0200

    03/04/2021 Finalización del proyecto

--- index.php
+++ index.php
@@ -8,10 +8,11 @@ require_once 'config/configDB.php';
 //Si el usuario ha solicitado otra página distinta del login
 if(isset($_SESSION['usuarioDAW2LoginLogoffMulticapaPOO'])){
     //Incluimos el controlador de la página solicitada almacenado en la sesión
-    require_once $_SESSION['paginaEnCurso']; 
-} else if (isset($_SESSION['paginaEnCursoSinRegistro'])){ // Si el usuario no ha iniciado sesion y a solicitado una pagina en curso sin registro
-    require_once $_SESSION['paginaEnCursoSinRegistro']; // Incluimos el controlador de la pagina en curso sin registro
-}else{ // Si el usuario no se ha identificado y no ha solicitado ninguna pagina en curso sin registro por defecto cargaremos el login
-    require_once $controladores['login']; // Incluimos el controlador del login
+    require_once $_SESSION['paginaEnCurso'];
+    //Si el usuario no ha iniciado sesión y ha solicitado una página en curso sin registro(opción registrarse o ventanas enlazadas al WIP) incluimos el controlador de esa página
+} else if (isset($_SESSION['paginaEnCursoSinRegistro'])){ 
+    require_once $_SESSION['paginaEnCursoSinRegistro'];
+}else{ //Si el usuario no se ha identificado y no ha solicitado ninguna página en curso sin registro por defecto cargaremos el login
+    require_once $controladores['login'];
 }
  
